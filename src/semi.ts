59ee7acd69ccdbe5d7cef4fb15b1266d5401153e
DDU <ddu6@protonmail.com>
2022-01-03 03:24:55 +0800

    update

--- src/semi.ts
+++ src/semi.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import {existsSync,readdirSync,writeFileSync} from 'fs'
-if(existsSync('icons/')){
-    let out=''
-    for(const file of readdirSync('icons')){
-        const [name]=file.split('.',1)
-        out+=`@font-face{font-family:"${name}";src:url(${file})}.show-icon.${name}::before{font-family:"${name}";content:"A"}\n`
+import {existsSync, readdirSync, writeFileSync} from 'fs'
+if (existsSync('icons/')) {
+    let out = ''
+    for (const file of readdirSync('icons')) {
+        const [name] = file.split('.', 1)
+        out += `@font-face{font-family:"${name}";src:url(${file})}.show-icon.${name}::before{font-family:"${name}";content:"A"}\n`
     }
-    writeFileSync('icons/main.css',out)
+    writeFileSync('icons/main.css', out)
 }
